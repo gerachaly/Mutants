945d7ea5f84ac6e9e41f24b0e22ff9ab5a7b508c
Gerardo Gomez <geracreepy14@gmail.com>
2023-11-15 21:04:37 -0300

    Bucle para la secuencia ADN con validaciones

--- TPMutants.py
+++ TPMutants.py
@@ -5,3 +5,15 @@ print("Ejemplo 'CGATTC")
 print("(En MAYUSCULAS)")
 letrasValidas = "ATCG"
 SecuenciaADN = []
+
+while len(SecuenciaADN) < 6:
+    print("Secuencia {}: ".format(len(SecuenciaADN)+1), end=" ")
+    entrada = input()
+    if len(entrada) !=6:
+        print("La secuencia tiene que tener solo 6 digitos.") ## aca se pide la secuencia de adn en forma de string
+    elif entrada.isdigit():                                   ## y se hacen las validaciones de no tener numeros y ser mayor a 6 digitos.
+        print("Error, la secuencia no puede tener numeros")
+    elif valida_letras(entrada, letrasValidas):
+        SecuenciaADN.append(list(entrada.upper()))
+    else:
+        print("La secuencia solo puede contener las letras (A, T, C, G).")
