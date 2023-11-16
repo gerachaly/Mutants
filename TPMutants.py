7c707e102d67d450337a8153e5610ba85c39e614
Gerardo Gomez <geracreepy14@gmail.com>
2023-11-15 21:11:24 -0300

    Funcion para imprimir toda la secuencia de adn, e idea para mostrar el resultado.

--- TPMutants.py
+++ TPMutants.py
@@ -23,3 +23,24 @@ def valida_letras(entrada, letras):
         if car not in letras:  ## Funcion que valida si la secuencia contiene las letras A,T,C,G
             return False       ## si contiene otras letras devuelve falso.
     return True
+
+resultado = isADN(SecuenciaADN)
+
+if resultado:       ## aca dependiendo el resultado es lo que dira.
+    print("Felicidades, es mutante!!")
+    print("Ahora puede pelear contra el pelado en la silla de ruedas.")
+else:
+    print("Lo sentimos, usted es cualquier cosa menos mutante.")
+
+
+def isADN(secuencia): ##esta funcion lo que hace es imprimir toda la secuencia de adn
+    esMutante = None    ## el llamado a la otra funcion es para recorrer el adn.
+    for filas in SecuenciaADN:
+        print("|",end="")
+        for col in filas:
+            print(col, end = "|")
+        print("")
+    esMutante = recorrerADN(secuencia)
+    return esMutante
+
+def recorrerADN(secuencia):
