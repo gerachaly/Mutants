4dc26120e2e3573691d63c00a986873b98e9c253
Gerardo Gomez <geracreepy14@gmail.com>
2023-11-15 21:01:43 -0300

    Cabecera del trabajo, con indicaciones y variables definidas

---
+++ TPMutants.py
@@ -0,0 +1,7 @@
+print("Bienvenido al reclutamiento de Mutantes!")
+print("Queremos probar su valentia y coraje para pelear contra los X-Men's")
+print("Le pido porfacor que ingrese su secuencia de ADN de longitud 6(A, T, C, G)")
+print("Ejemplo 'CGATTC")
+print("(En MAYUSCULAS)")
+letrasValidas = "ATCG"
+SecuenciaADN = []
